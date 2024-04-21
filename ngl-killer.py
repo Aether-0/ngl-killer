85687a2121471900e02424eb4687fe5f71f8775d
Aether-0 <143575444+Aether-0@users.noreply.github.com>
2024-04-21 23:08:22 +0630

    random all

--- ngl-killer.py
+++ ngl-killer.py
@@ -1,7 +1,9 @@
-#crd to https://github.com/0MeMo07
+# crd to https://github.com/0MeMo07
 import requests
 import time
 import re
+import random
+
 
 class Colors:
     light_blue = '\033[94m'
@@ -10,11 +12,11 @@ class Colors:
     red = '\033[91m'
     end = '\033[0m'
 
+
 class Colorate:
     @staticmethod
     def Vertical(color, text):
         return f"{color}{text}{Colors.end}"
-
 def extract_username(url):
     pattern = r"https://[^/]+/(\w+)"
     match = re.match(pattern, url)
@@ -23,9 +25,10 @@ def extract_username(url):
     else:
         raise ValueError("Invalid URL format")
 
+
 def print_banner():
     banner = '''
-    
+
  .-. .-.  ,--,   ,-.         ,-. .-.,-.,-.    ,-.    ,---.  ,---.    
  |  \| |.' .'    | |         | |/ / |(|| |    | |    | .-'  | .-.\   
  |   | ||  |  __ | |____.___ | | /  (_)| |    | |    | `-.  | `-'/   
@@ -33,9 +36,28 @@ def print_banner():
  | | |)| \  `-) )| `--.      | |) \ | || `--. | `--. |  `--.| |\ \   
  /(  (_) )\____/ |( __.'     |((_)-'`-'|( __.'|( __.'/( __.'|_| \)\  
 (__)    (__)     (_)         (_)       (_)    (_)   (__)        (__) 
-                                  
+
     '''
     print(Colorate.Vertical(Colors.green, banner))
+
+
+
+
+def generate_random_string(length):
+    characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def generate_random_uuid():
+    segments = [
+        generate_random_string(8),
+        generate_random_string(4),
+        generate_random_string(4),
+        generate_random_string(4),
+        generate_random_string(12)
+    ]
+    return '-'.join(segments)
+
+
 
 def print_author_info():
     author_info = '''
@@ -49,10 +71,10 @@ def print_author_info():
     '''
     print(Colorate.Vertical(Colors.golden, author_info))
 
+
 try:
     print_banner()
     print_author_info()
-
     url = input(Colorate.Vertical(Colors.light_blue, "[🔗]Url: "))
     nglusername = extract_username(url)
     message = input(Colorate.Vertical(Colors.light_blue, "[💬]Message: "))
@@ -60,6 +82,17 @@ try:
 
     value = 0
     not_send = 0
+
+    ug = [
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9'
+    ]
+
+    random_user_agent = random.choice(ug)
+    random_uuid = generate_random_uuid()
     while value < count:
         headers = {
             'Host': 'ngl.link',
@@ -68,7 +101,7 @@ try:
             'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'x-requested-with': 'XMLHttpRequest',
             'sec-ch-ua-mobile': '?0',
-            'user-agent': 'ngl-killer',
+            'user-agent': f'{random_user_agent}',
             'sec-ch-ua-platform': '"Windows"',
             'origin': 'https://ngl.link',
             'sec-fetch-site': 'same-origin',
@@ -81,13 +114,13 @@ try:
         data = {
             'username': nglusername,
             'question': message,
-            'deviceId': '0',
+            'deviceId': f'{random_uuid}',
             'gameSlug': '',
             'referrer': '',
         }
 
         try:
-            response = requests.post('https://ngl.link/api/submit', headers=headers, data=data)
+            response = requests.post('https://ngl.link/api/submit', headers=headers, data=data, )
             response.raise_for_status()  # Raise an HTTPError for bad responses
             if response.status_code == 200:
                 not_send = 0
@@ -107,3 +140,4 @@ except KeyboardInterrupt:
     print(Colorate.Vertical(Colors.red, "\nUser Interrupted"))
 except Exception as e:
     print(Colorate.Vertical(Colors.red, f"An unexpected error occurred: {e}"))
+    
